7e3437308ef5cf4bcc1911f703f1004bc4587ba0
Vincent Huber <vincent.huber@cemosis.fr>
2015-08-04 18:30:44 +0200

    import tutorial from feelpp

--- Tutorial/code/03-mymesh.cpp
+++ Tutorial/code/03-mymesh.cpp
@@ -4,6 +4,7 @@ using namespace Feel;
 
 int main( int argc, char** argv )
 {
+    using namespace Feel;
     // initialize Feel++ Environment
     Environment env( _argc=argc, _argv=argv,
                      _about=about( _name="mymesh" ,
@@ -11,4 +12,9 @@ int main( int argc, char** argv )
                                    _email="feelpp-devel@feelpp.org" ) );
     // create a mesh with GMSH using Feel++ geometry tool
     auto mesh = loadMesh(_mesh=new  Mesh<Simplex<2>>);
+
+    LOG(INFO) << "mesh " << soption(_name="gmsh.filename") << " loaded";
+
+    LOG(INFO) << "volume =" << integrate( elements( mesh ), cst( 1. ) ).evaluate();
+    LOG(INFO) << "surface = " << integrate( boundaryfaces( mesh ), cst( 1. ) ).evaluate();
 }
